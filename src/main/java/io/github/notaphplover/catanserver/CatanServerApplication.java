dab793e6411bc66bdbd11f8acd1b800f4393b7a7
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2020-05-23 14:10:11 +0200

    feat(boot): add server boot

---
+++ src/main/java/io/github/notaphplover/catanserver/CatanServerApplication.java
@@ -0,0 +1,13 @@
+package io.github.notaphplover.catanserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatanServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatanServerApplication.class, args);
+	}
+
+}
