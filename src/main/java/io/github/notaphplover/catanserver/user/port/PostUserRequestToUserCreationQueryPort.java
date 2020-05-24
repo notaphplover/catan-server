4a7e84d0c7d86bb2d1239b12ac9b1eb504badeba
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2020-05-24 23:31:04 +0200

    refactor(user): PostUserRequestToUserCreationQuery is now a service

--- src/main/java/io/github/notaphplover/catanserver/user/port/PostUserRequestToUserCreationQueryPort.java
+++ src/main/java/io/github/notaphplover/catanserver/user/port/PostUserRequestToUserCreationQueryPort.java
@@ -1,9 +1,12 @@
 package io.github.notaphplover.catanserver.user.port;
 
+import org.springframework.stereotype.Service;
+
 import io.github.notaphplover.catanserver.common.port.IPort;
 import io.github.notaphplover.catanserver.user.adapter.api.request.PostUserRequest;
 import io.github.notaphplover.catanserver.user.domain.query.UserCreationQuery;
 
+@Service
 public class PostUserRequestToUserCreationQueryPort implements IPort<PostUserRequest, UserCreationQuery> {
 
     @Override
