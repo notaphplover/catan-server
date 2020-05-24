65cb86265ed4d32511c841aff56750ca13b418dc
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2020-05-24 17:42:06 +0200

    feat(user): Add post user request

---
+++ src/main/java/io/github/notaphplover/catanserver/user/adapter/api/request/PostUserRequest.java
@@ -0,0 +1,23 @@
+package io.github.notaphplover.catanserver.user.adapter.api.request;
+
+public class PostUserRequest {
+    private String username;
+
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
