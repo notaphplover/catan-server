311a92f6ec28e36affa7b7047b2ce4e114d3c626
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2020-05-24 18:01:27 +0200

    feat(user) add PostUserRequestToUserCreationQueryPort

---
+++ src/main/java/io/github/notaphplover/catanserver/user/port/PostUserRequestToUserCreationQueryPort.java
@@ -0,0 +1,14 @@
+package io.github.notaphplover.catanserver.user.port;
+
+import io.github.notaphplover.catanserver.common.port.IPort;
+import io.github.notaphplover.catanserver.user.adapter.api.request.PostUserRequest;
+import io.github.notaphplover.catanserver.user.domain.query.UserCreationQuery;
+
+public class PostUserRequestToUserCreationQueryPort implements IPort<PostUserRequest, UserCreationQuery> {
+
+    @Override
+    public UserCreationQuery transform(PostUserRequest input) {
+        return new UserCreationQuery(input.getUsername(), input.getPassword());
+    }
+    
+}
