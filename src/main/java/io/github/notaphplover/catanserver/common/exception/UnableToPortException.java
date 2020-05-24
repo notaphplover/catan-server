70c92b1f6f215e063dd7d357ec15b5930a001256
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2020-05-24 17:24:05 +0200

    feat(common): add UnableToPortException

---
+++ src/main/java/io/github/notaphplover/catanserver/common/exception/UnableToPortException.java
@@ -0,0 +1,10 @@
+package io.github.notaphplover.catanserver.common.exception;
+
+public class UnableToPortException extends RuntimeException {
+
+    private static final long serialVersionUID = 5479253370158897792L;
+
+    public UnableToPortException(Exception cause) {
+        super(cause);
+    }
+}
