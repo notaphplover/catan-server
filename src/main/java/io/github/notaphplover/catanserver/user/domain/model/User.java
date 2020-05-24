64ece9fc048ec02b31dff56107153840cc877cb2
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2020-05-24 15:26:33 +0200

    feat(user): add user domain model

---
+++ src/main/java/io/github/notaphplover/catanserver/user/domain/model/User.java
@@ -0,0 +1,43 @@
+package io.github.notaphplover.catanserver.user.domain.model;
+
+public class User implements IUser {
+    
+    private Long id;
+
+    private String username;
+
+    private String passwordHash;
+
+    public User(Long id) {
+        this.id = id;
+    }
+
+    public User() {
+       this(null); 
+    }
+
+    @Override
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+
+    @Override
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
