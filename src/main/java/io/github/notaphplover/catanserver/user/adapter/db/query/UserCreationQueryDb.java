71a33416022498ac043bf713cc72f9ff286b521d
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2020-05-25 17:34:07 +0200

    feat(user): add user creation query db model

---
+++ src/main/java/io/github/notaphplover/catanserver/user/adapter/db/query/UserCreationQueryDb.java
@@ -0,0 +1,21 @@
+package io.github.notaphplover.catanserver.user.adapter.db.query;
+
+public class UserCreationQueryDb {
+    
+    private final String username;
+
+    private final String passwordHash;
+
+    public UserCreationQueryDb(String username, String passwordHash) {
+        this.username = username;
+        this.passwordHash = passwordHash;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+}
