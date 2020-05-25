4cf6d854af9aec4a9eaca17012d0c989f7e699a3
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2020-05-25 17:31:15 +0200

    feat(user): Add UserCreationQueryToUserCreationQueryDbPort

---
+++ src/main/java/io/github/notaphplover/catanserver/user/port/UserCreationQueryToUserCreationQueryDbPort.java
@@ -0,0 +1,29 @@
+package io.github.notaphplover.catanserver.user.port;
+
+import org.springframework.stereotype.Service;
+
+import io.github.notaphplover.catanserver.common.port.IPort;
+import io.github.notaphplover.catanserver.user.adapter.db.query.UserCreationQueryDb;
+import io.github.notaphplover.catanserver.user.domain.interactor.HashPasswordInteractor;
+import io.github.notaphplover.catanserver.user.domain.query.UserCreationQuery;
+
+@Service
+public class UserCreationQueryToUserCreationQueryDbPort implements IPort<UserCreationQuery, UserCreationQueryDb> {
+
+    private HashPasswordInteractor hashPasswordInteractor;
+
+    public UserCreationQueryToUserCreationQueryDbPort(HashPasswordInteractor hashPasswordInteractor) {
+        this.hashPasswordInteractor = hashPasswordInteractor;
+    }
+
+    @Override
+    public UserCreationQueryDb transform(UserCreationQuery input) {
+        
+        String username = input.getUsername();
+        String passwordHash = hashPasswordInteractor.interact(input.getPassword());
+
+        return new UserCreationQueryDb(username, passwordHash);
+    }
+
+}
+
