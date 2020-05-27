30e953488f27d13b38e9680c728a9f3214e5d6e8
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2020-05-27 20:19:02 +0200

    feat(user): add validation annotations to post user request model

--- src/main/java/io/github/notaphplover/catanserver/user/adapter/api/request/PostUserRequest.java
+++ src/main/java/io/github/notaphplover/catanserver/user/adapter/api/request/PostUserRequest.java
@@ -1,8 +1,19 @@
 package io.github.notaphplover.catanserver.user.adapter.api.request;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class PostUserRequest {
+
+    @Size(min = 6, max = 128)
+    @NotBlank
+    @NotNull
     private String username;
 
+    @Size(min = 8, max = 128)
+    @NotBlank
+    @NotNull
     private String password;
 
     public String getUsername() {
