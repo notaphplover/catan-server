232fcacb47e8ac5d247ff584c35f2f5a99c5c894
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2020-05-27 21:14:41 +0200

    refactor(user): remove unused password user api field

--- src/main/java/io/github/notaphplover/catanserver/user/adapter/api/model/IUserApi.java
+++ src/main/java/io/github/notaphplover/catanserver/user/adapter/api/model/IUserApi.java
@@ -3,9 +3,5 @@ package io.github.notaphplover.catanserver.user.adapter.api.model;
 public interface IUserApi {
     String getUsername();
 
-    String getPassword();
-
     void setUsername(String username);
-
-    void setPassword(String password);
 }
