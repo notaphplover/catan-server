9deb2ca3f17ba8072d9130c3b2fce1a6780812f6
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2020-05-27 21:26:49 +0200

    feat(user): add unique constraint at username db field

--- src/main/java/io/github/notaphplover/catanserver/user/adapter/db/model/UserDb.java
+++ src/main/java/io/github/notaphplover/catanserver/user/adapter/db/model/UserDb.java
@@ -4,8 +4,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 
 @Entity
+@Table(
+  uniqueConstraints=
+      @UniqueConstraint(columnNames={ "username" })
+)
 public class UserDb {
     
   @Id
