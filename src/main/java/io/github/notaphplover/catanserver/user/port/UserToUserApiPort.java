9c7d0031d8700e934259781e97e8c4379cc121d2
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2020-05-27 21:14:03 +0200

    refactor(user): change UserToUserApiPort to operate with IUserApi instead of UserApi

--- src/main/java/io/github/notaphplover/catanserver/user/port/UserToUserApiPort.java
+++ src/main/java/io/github/notaphplover/catanserver/user/port/UserToUserApiPort.java
@@ -3,14 +3,15 @@ package io.github.notaphplover.catanserver.user.port;
 import org.springframework.stereotype.Service;
 
 import io.github.notaphplover.catanserver.common.port.IPort;
+import io.github.notaphplover.catanserver.user.adapter.api.model.IUserApi;
 import io.github.notaphplover.catanserver.user.adapter.api.model.UserApi;
 import io.github.notaphplover.catanserver.user.domain.model.IUser;
 
 @Service
-public class UserToUserApiPort implements IPort<IUser, UserApi> {
+public class UserToUserApiPort implements IPort<IUser, IUserApi> {
 
     @Override
-    public UserApi transform(IUser input) {
+    public IUserApi transform(IUser input) {
         UserApi userApi = new UserApi();
         userApi.setUsername(input.getUsername());
         
