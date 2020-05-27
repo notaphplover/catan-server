49b169dacf4d80eec18207616fc4f36f3a6aef5e
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2020-05-27 20:19:52 +0200

    feat(user): add post user request handler

--- src/main/java/io/github/notaphplover/catanserver/user/adapter/api/reqHandler/PostUserReqHandler.java
+++ src/main/java/io/github/notaphplover/catanserver/user/adapter/api/reqHandler/PostUserReqHandler.java
@@ -1,14 +1,36 @@
 package io.github.notaphplover.catanserver.user.adapter.api.reqHandler;
 
+import org.springframework.stereotype.Service;
+
 import io.github.notaphplover.catanserver.common.adapter.api.reqHandler.IReqHandler;
 import io.github.notaphplover.catanserver.user.adapter.api.model.UserApi;
 import io.github.notaphplover.catanserver.user.adapter.api.request.PostUserRequest;
+import io.github.notaphplover.catanserver.user.domain.interactor.CreateUserInteractor;
+import io.github.notaphplover.catanserver.user.domain.model.IUser;
+import io.github.notaphplover.catanserver.user.domain.query.UserCreationQuery;
+import io.github.notaphplover.catanserver.user.port.PostUserRequestToUserCreationQueryPort;
+import io.github.notaphplover.catanserver.user.port.UserToUserApiPort;
 
+@Service
 public class PostUserReqHandler implements IReqHandler<PostUserRequest, UserApi> {
+
+    private CreateUserInteractor createUserInteractor;
+
+    private PostUserRequestToUserCreationQueryPort postUserRequestToUserCreationQueryPort;
+
+    private UserToUserApiPort userToUserApiPort;
+
+    public PostUserReqHandler(CreateUserInteractor createUserInteractor, PostUserRequestToUserCreationQueryPort postUserRequestToUserCreationQueryPort, UserToUserApiPort userToUserApiPort) {
+        this.createUserInteractor = createUserInteractor;
+        this.postUserRequestToUserCreationQueryPort = postUserRequestToUserCreationQueryPort;
+        this.userToUserApiPort = userToUserApiPort;
+    }
 
     @Override
     public UserApi handle(PostUserRequest request) {
-        return null;
+        UserCreationQuery creationQuery = postUserRequestToUserCreationQueryPort.transform(request);
+        IUser userCreated = createUserInteractor.interact(creationQuery);
+        return userToUserApiPort.transform(userCreated);
     }
 
 }
