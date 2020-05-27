06911eb131eba48a8978742bc8f4c938d5478308
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2020-05-27 20:20:16 +0200

    feat(user): add user controller

---
+++ src/main/java/io/github/notaphplover/catanserver/user/adapter/api/controller/UserController.java
@@ -0,0 +1,30 @@
+package io.github.notaphplover.catanserver.user.adapter.api.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.github.notaphplover.catanserver.user.adapter.api.model.UserApi;
+import io.github.notaphplover.catanserver.user.adapter.api.reqHandler.PostUserReqHandler;
+import io.github.notaphplover.catanserver.user.adapter.api.request.PostUserRequest;
+
+@RestController
+@RequestMapping("/users")
+@Validated
+public class UserController {
+
+  private PostUserReqHandler postUserReqHandler;
+
+  public UserController(PostUserReqHandler postUserReqHandler) {
+    this.postUserReqHandler = postUserReqHandler;
+  }
+    
+  @PostMapping()
+  UserApi newUser(@Valid @RequestBody PostUserRequest postUserRequest) {
+    return this.postUserReqHandler.handle(postUserRequest);
+  }
+}
