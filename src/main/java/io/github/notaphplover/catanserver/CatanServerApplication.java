428be7494e35aecc2af3c34e45e7384d0d1c98bd
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2020-05-30 23:54:26 +0200

    feat(boot): add filters to avoid loading undesired entities

--- src/main/java/io/github/notaphplover/catanserver/CatanServerApplication.java
+++ src/main/java/io/github/notaphplover/catanserver/CatanServerApplication.java
@@ -2,7 +2,9 @@ package io.github.notaphplover.catanserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
+@EntityScan(basePackages = {"io.github.notaphplover.catanserver.user.adapter.db.model"})
 @SpringBootApplication
 public class CatanServerApplication {
 
