4b2f1399d619197151e293f3598d0bea19823ac5
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2020-05-30 13:19:12 +0200

    feat(user): add  user repository interface

--- src/main/java/io/github/notaphplover/catanserver/user/adapter/db/repository/IUserRepository.java
+++ src/main/java/io/github/notaphplover/catanserver/user/adapter/db/repository/IUserRepository.java
@@ -2,5 +2,6 @@ package io.github.notaphplover.catanserver.user.adapter.db.repository;
 
 import io.github.notaphplover.catanserver.user.adapter.db.model.UserDb;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface IUserRepository extends JpaRepository<UserDb, Long> {}
+public interface IUserRepository extends JpaRepository<UserDb, Long>, JpaSpecificationExecutor<UserDb> {}
