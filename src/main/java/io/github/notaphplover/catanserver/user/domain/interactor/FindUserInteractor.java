5204c7d1718f785efa950c4331ed3658465fe6bf
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2020-05-30 13:16:48 +0200

    feat(user): add find user interactor

---
+++ src/main/java/io/github/notaphplover/catanserver/user/domain/interactor/FindUserInteractor.java
@@ -0,0 +1,26 @@
+package io.github.notaphplover.catanserver.user.domain.interactor;
+
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import io.github.notaphplover.catanserver.common.domain.interactor.IInteractor;
+import io.github.notaphplover.catanserver.user.adapter.db.repository.UserRepositoryManager;
+import io.github.notaphplover.catanserver.user.domain.model.IUser;
+import io.github.notaphplover.catanserver.user.domain.query.UserFindQuery;
+
+@Service
+public class FindUserInteractor implements IInteractor<UserFindQuery, Optional<IUser>> {
+
+    private UserRepositoryManager userRepositoryManager;
+
+    public FindUserInteractor(UserRepositoryManager userRepositoryManager) {
+        this.userRepositoryManager = userRepositoryManager;
+    }
+
+    @Override
+    public Optional<IUser> interact(UserFindQuery input) {
+        return userRepositoryManager.find(input);
+    }
+    
+}
