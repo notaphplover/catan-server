256b99c3c20c237943f27870f65d7e021bf8fcca
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2020-05-31 00:17:49 +0200

    feat(common): add request filter

---
+++ src/main/java/io/github/notaphplover/catanserver/common/adapter/api/config/JwtRequestFilter.java
@@ -0,0 +1,61 @@
+package io.github.notaphplover.catanserver.common.adapter.api.config;
+
+import io.github.notaphplover.catanserver.user.adapter.jwt.JwtManager;
+import io.github.notaphplover.catanserver.user.adapter.jwt.model.UserTokenJwt;
+import io.github.notaphplover.catanserver.user.domain.model.IUserToken;
+import io.github.notaphplover.catanserver.user.port.UserTokenJwtToUserTokenPort;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.Optional;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Component
+public class JwtRequestFilter extends OncePerRequestFilter {
+
+  @Autowired private JwtManager jwtTokenUtil;
+
+  @Autowired UserTokenJwtToUserTokenPort userTokenJwtToUserTokenPort;
+
+  @Override
+  protected void doFilterInternal(
+      HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+      throws ServletException, IOException {
+    final String requestTokenHeader = request.getHeader("Authorization");
+
+    String jwtToken = null;
+
+    Optional<UserTokenJwt> userTokenJwtCapsule = null;
+
+    if (requestTokenHeader != null && requestTokenHeader.startsWith("Bearer ")) {
+      jwtToken = requestTokenHeader.substring(7);
+      userTokenJwtCapsule = jwtTokenUtil.validateAndGet(jwtToken);
+    } else {
+      logger.warn("JWT Token does not begin with Bearer String");
+    }
+
+    if (userTokenJwtCapsule != null
+        && userTokenJwtCapsule.isPresent()
+        && SecurityContextHolder.getContext().getAuthentication() == null) {
+      IUserToken userToken = userTokenJwtToUserTokenPort.transform(userTokenJwtCapsule.get());
+
+      UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken =
+          new UsernamePasswordAuthenticationToken(
+              userToken, null, new LinkedList<GrantedAuthority>());
+      usernamePasswordAuthenticationToken.setDetails(
+          new WebAuthenticationDetailsSource().buildDetails(request));
+
+      SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+    }
+    chain.doFilter(request, response);
+  }
+}
