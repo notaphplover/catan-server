ab56b95ae934179ea21ae67dd4f90ebfce34d9e0
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2020-05-31 16:13:18 +0200

    style(security): apply spotless suggestions

--- src/test/java/io/github/notaphplover/catanserver/security/adapter/AppPasswordEncoderTest.java
+++ src/test/java/io/github/notaphplover/catanserver/security/adapter/AppPasswordEncoderTest.java
@@ -18,55 +18,55 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @DisplayName("ApiAuthenticationEntrypoint tests")
 @TestInstance(Lifecycle.PER_CLASS)
 public class AppPasswordEncoderTest {
-    
-    PasswordEncoder passwordEncoder = null;
 
-    AppPasswordEncoder appPasswordEncoder = null;
+  PasswordEncoder passwordEncoder = null;
+
+  AppPasswordEncoder appPasswordEncoder = null;
+
+  @BeforeAll
+  public void beforeAll() {
+    passwordEncoder = Mockito.mock(PasswordEncoder.class);
+
+    appPasswordEncoder = new AppPasswordEncoder(passwordEncoder);
+  }
+
+  @Nested
+  @DisplayName("AppPasswordEncoder.encode")
+  @TestInstance(Lifecycle.PER_CLASS)
+  class Encode {
+
+    private String passwordEncoderEncodeReturnValue = "passwordEncoderEncode.encode return value";
 
     @BeforeAll
     public void beforeAll() {
-        passwordEncoder = Mockito.mock(PasswordEncoder.class);
-
-        appPasswordEncoder = new AppPasswordEncoder(passwordEncoder);
+      when(passwordEncoder.encode(anyString())).thenReturn(passwordEncoderEncodeReturnValue);
     }
 
     @Nested
-    @DisplayName("AppPasswordEncoder.encode")
+    @DisplayName("AppPasswordEncoder.encode, when called")
     @TestInstance(Lifecycle.PER_CLASS)
-    class Encode {
+    class WhenCalled {
 
-        private String passwordEncoderEncodeReturnValue = "passwordEncoderEncode.encode return value"; 
+      private String passwordParam = null;
+      private Object result = null;
 
-        @BeforeAll
-        public void beforeAll() {
-            when(passwordEncoder.encode(anyString())).thenReturn(passwordEncoderEncodeReturnValue);
-        }
+      @BeforeAll
+      public void beforeAll() {
+        passwordParam = "password";
+        result = appPasswordEncoder.encode(passwordParam);
+      }
 
-        @Nested
-        @DisplayName("AppPasswordEncoder.encode, when called")
-        @TestInstance(Lifecycle.PER_CLASS)
-        class WhenCalled {
+      @DisplayName("It must call PasswordEncoder.encode with the password received")
+      @Test
+      public void itMustCallPasswordEncoder() {
+        verify(passwordEncoder).encode(eq(passwordParam));
+      }
 
-            private String passwordParam = null;
-            private Object result = null;
-
-            @BeforeAll
-            public void beforeAll() {
-                passwordParam = "password";
-                result = appPasswordEncoder.encode(passwordParam);
-            }
-
-            @DisplayName("It must call PasswordEncoder.encode with the password received")
-            @Test
-            public void itMustCallPasswordEncoder() {
-                verify(passwordEncoder).encode(eq(passwordParam));
-            }
-
-            @DisplayName("It must call return the encoded value")
-            @Test
-            public void itMustReturnTheEncodedValue() {
-                assertSame(passwordEncoderEncodeReturnValue, result);
-            }
-        }
+      @DisplayName("It must call return the encoded value")
+      @Test
+      public void itMustReturnTheEncodedValue() {
+        assertSame(passwordEncoderEncodeReturnValue, result);
+      }
     }
+  }
 }
