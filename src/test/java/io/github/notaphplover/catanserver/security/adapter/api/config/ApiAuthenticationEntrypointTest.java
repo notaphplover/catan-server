38f7f6d7f0a1c1ca2e90d5216fa5c8989c2a4665
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2020-05-31 16:13:59 +0200

    test(security): Nest ApiAuthenticationEntrypoint.commence tests in a WhenCalled class

--- src/test/java/io/github/notaphplover/catanserver/security/adapter/api/config/ApiAuthenticationEntrypointTest.java
+++ src/test/java/io/github/notaphplover/catanserver/security/adapter/api/config/ApiAuthenticationEntrypointTest.java
@@ -5,10 +5,8 @@ import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
 
 import java.io.IOException;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -21,26 +19,34 @@ import org.springframework.security.core.AuthenticationException;
 @DisplayName("ApiAuthenticationEntrypoint tests")
 public class ApiAuthenticationEntrypointTest {
 
-  private ApiAuthenticationEntrypoint apiAuthenticationEntrypoint = new ApiAuthenticationEntrypoint();
+  private ApiAuthenticationEntrypoint apiAuthenticationEntrypoint =
+      new ApiAuthenticationEntrypoint();
 
   @Nested
-  @DisplayName("Agent.commence")
+  @DisplayName("ApiAuthenticationEntrypoint.commence")
   @TestInstance(Lifecycle.PER_CLASS)
   class Commence {
 
-    private HttpServletRequest requestMock = Mockito.mock(HttpServletRequest.class);
-    private HttpServletResponse responseMock = Mockito.mock(HttpServletResponse.class);
-    private AuthenticationException authExceptionMock = Mockito.mock(AuthenticationException.class);
+    @Nested
+    @DisplayName("ApiAuthenticationEntrypoint.commence, when called")
+    @TestInstance(Lifecycle.PER_CLASS)
+    class WhenCalled {
 
-    @BeforeAll
-    public void beforeAll() throws IOException {
-      apiAuthenticationEntrypoint.commence(requestMock, responseMock, authExceptionMock);
-    }
+      private HttpServletRequest requestMock = Mockito.mock(HttpServletRequest.class);
+      private HttpServletResponse responseMock = Mockito.mock(HttpServletResponse.class);
+      private AuthenticationException authExceptionMock =
+          Mockito.mock(AuthenticationException.class);
 
-    @DisplayName("it must call HttpServletResponse.sendError with an unauthorized status code")
-    @Test
-    public void itMustCallHttpServletResponse() throws IOException {
-      verify(responseMock).sendError(eq(HttpServletResponse.SC_UNAUTHORIZED), any());
+      @BeforeAll
+      public void beforeAll() throws IOException {
+        apiAuthenticationEntrypoint.commence(requestMock, responseMock, authExceptionMock);
+      }
+
+      @DisplayName("it must call HttpServletResponse.sendError with an unauthorized status code")
+      @Test
+      public void itMustCallHttpServletResponse() throws IOException {
+        verify(responseMock).sendError(eq(HttpServletResponse.SC_UNAUTHORIZED), any());
+      }
     }
   }
 }
