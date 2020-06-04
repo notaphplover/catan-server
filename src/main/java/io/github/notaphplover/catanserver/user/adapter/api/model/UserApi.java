475232b4d9d094558bf9ba2ba88b2896d14c6502
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2020-06-04 18:30:58 +0200

    feat(user): Add equals method to UserApi

--- src/main/java/io/github/notaphplover/catanserver/user/adapter/api/model/UserApi.java
+++ src/main/java/io/github/notaphplover/catanserver/user/adapter/api/model/UserApi.java
@@ -10,6 +10,16 @@ public class UserApi implements IUserApi {
     this.username = username;
   }
 
+  @Override
+  public boolean equals(Object obj) {
+    if (!(obj instanceof IUserApi)) {
+      return false;
+    }
+
+    return ((IUserApi) obj).getToken().equals(getToken())
+        && ((IUserApi) obj).getUsername().equals(getUsername());
+  }
+
   public String getToken() {
     return token;
   }
