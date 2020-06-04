1f1be15ef315de1106ce04fbff91c28114cb70e9
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2020-06-04 19:04:04 +0200

    refactor(user): change PostUserReqHandler to depend on interfaces instead of classes

--- src/main/java/io/github/notaphplover/catanserver/user/adapter/api/reqHandler/PostUserReqHandler.java
+++ src/main/java/io/github/notaphplover/catanserver/user/adapter/api/reqHandler/PostUserReqHandler.java
@@ -1,28 +1,28 @@
 package io.github.notaphplover.catanserver.user.adapter.api.reqHandler;
 
 import io.github.notaphplover.catanserver.common.adapter.api.reqHandler.IReqHandler;
+import io.github.notaphplover.catanserver.common.domain.interactor.IInteractor;
+import io.github.notaphplover.catanserver.common.port.IPort;
 import io.github.notaphplover.catanserver.user.adapter.api.model.IUserApi;
 import io.github.notaphplover.catanserver.user.adapter.api.request.PostUserRequest;
-import io.github.notaphplover.catanserver.user.domain.interactor.CreateUserInteractor;
 import io.github.notaphplover.catanserver.user.domain.model.IUser;
 import io.github.notaphplover.catanserver.user.domain.query.UserCreationQuery;
 import io.github.notaphplover.catanserver.user.port.PostUserRequestToUserCreationQueryPort;
-import io.github.notaphplover.catanserver.user.port.UserToUserApiPort;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PostUserReqHandler implements IReqHandler<PostUserRequest, IUserApi> {
 
-  private CreateUserInteractor createUserInteractor;
+  private IInteractor<UserCreationQuery, IUser> createUserInteractor;
 
   private PostUserRequestToUserCreationQueryPort postUserRequestToUserCreationQueryPort;
 
-  private UserToUserApiPort userToUserApiPort;
+  private IPort<IUser, IUserApi> userToUserApiPort;
 
   public PostUserReqHandler(
-      CreateUserInteractor createUserInteractor,
+      IInteractor<UserCreationQuery, IUser> createUserInteractor,
       PostUserRequestToUserCreationQueryPort postUserRequestToUserCreationQueryPort,
-      UserToUserApiPort userToUserApiPort) {
+      IPort<IUser, IUserApi> userToUserApiPort) {
     this.createUserInteractor = createUserInteractor;
     this.postUserRequestToUserCreationQueryPort = postUserRequestToUserCreationQueryPort;
     this.userToUserApiPort = userToUserApiPort;
