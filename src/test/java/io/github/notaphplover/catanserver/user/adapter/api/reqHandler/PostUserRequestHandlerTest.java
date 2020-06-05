1e7099be442c5185628a6c15c0e9f73c3163ae2f
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2020-06-05 22:13:23 +0200

    test(user): add PostUserRequestHandlerTest

---
+++ src/test/java/io/github/notaphplover/catanserver/user/adapter/api/reqHandler/PostUserRequestHandlerTest.java
@@ -0,0 +1,107 @@
+package io.github.notaphplover.catanserver.user.adapter.api.reqHandler;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import io.github.notaphplover.catanserver.common.domain.interactor.IInteractor;
+import io.github.notaphplover.catanserver.common.port.IPort;
+import io.github.notaphplover.catanserver.user.adapter.api.model.IUserApi;
+import io.github.notaphplover.catanserver.user.adapter.api.request.PostUserRequest;
+import io.github.notaphplover.catanserver.user.adapter.api.request.PostUserRequestFixturesUtils;
+import io.github.notaphplover.catanserver.user.adapter.model.UserApiFixturesUtils;
+import io.github.notaphplover.catanserver.user.domain.model.IUser;
+import io.github.notaphplover.catanserver.user.domain.model.UserFixturesUtils;
+import io.github.notaphplover.catanserver.user.domain.query.UserCreationQuery;
+import io.github.notaphplover.catanserver.user.domain.query.UserCreationQueryFixturesUtils;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.mockito.Mockito;
+
+@DisplayName("PostUserRequestHandler tests")
+@TestInstance(Lifecycle.PER_CLASS)
+public class PostUserRequestHandlerTest {
+
+  private IInteractor<UserCreationQuery, IUser> createUserInteractor = null;
+
+  private IPort<PostUserRequest, UserCreationQuery> postUserRequestToUserCreationQueryPort;
+
+  private IPort<IUser, IUserApi> userToUserApiPort = null;
+
+  private PostUserReqHandler postUserRequestHandler = null;
+
+  @BeforeAll
+  @SuppressWarnings("unchecked")
+  public void beforeAll() {
+    createUserInteractor = (IInteractor<UserCreationQuery, IUser>) Mockito.mock(IInteractor.class);
+
+    postUserRequestToUserCreationQueryPort =
+        (IPort<PostUserRequest, UserCreationQuery>) Mockito.mock(IPort.class);
+
+    userToUserApiPort = (IPort<IUser, IUserApi>) Mockito.mock(IPort.class);
+
+    postUserRequestHandler =
+        new PostUserReqHandler(
+            createUserInteractor, postUserRequestToUserCreationQueryPort, userToUserApiPort);
+  }
+
+  @Nested
+  @DisplayName("PostUserRequestHandler.handle")
+  @TestInstance(Lifecycle.PER_CLASS)
+  class Handle {
+
+    @BeforeAll
+    public void beforeAll() {
+      when(createUserInteractor.interact(any()))
+          .thenReturn(UserFixturesUtils.getUserFactory().get());
+      when(postUserRequestToUserCreationQueryPort.transform(any()))
+          .thenReturn(UserCreationQueryFixturesUtils.getUserCreationQueryFactory().get());
+      when(userToUserApiPort.transform(any()))
+          .thenReturn(UserApiFixturesUtils.getUserApiFactory().get());
+    }
+
+    @Nested
+    @DisplayName("PostUserRequestHandler.handle, when called")
+    @TestInstance(Lifecycle.PER_CLASS)
+    class WhenCalled {
+
+      private Object result = null;
+
+      @BeforeAll
+      public void beforeAll() {
+        result =
+            postUserRequestHandler.handle(
+                PostUserRequestFixturesUtils.postUserRequestFactory().get());
+      }
+
+      @DisplayName("It must call CreateUserInteractor.interact with the CreateUserQuery received")
+      @Test()
+      public void itMustCallCreateUserInteractor() {
+        verify(createUserInteractor)
+            .interact(eq(UserCreationQueryFixturesUtils.getUserCreationQueryFactory().get()));
+      }
+
+      @DisplayName(
+          "It must call PostUserRequestToUserCreationQueryPort.transform with the PostUserRequest received")
+      @Test()
+      public void itMustCallPostUserRequestToUserCreationQueryPort() {
+        verify(postUserRequestToUserCreationQueryPort)
+            .transform(eq(PostUserRequestFixturesUtils.postUserRequestFactory().get()));
+      }
+
+      @DisplayName("It must call UserToUserApiPort.transform with the IUser found")
+      @Test
+      public void itMustCallUserToApiPort() {
+        verify(userToUserApiPort).transform(eq(UserFixturesUtils.getUserFactory().get()));
+      }
+
+      @DisplayName("It must return the expected user")
+      @Test
+      public void itMustReturnAUser() {
+        assertEquals(UserApiFixturesUtils.getUserApiFactory().get(), result);
+      }
+    }
+  }
+}
