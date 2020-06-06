7cb1a77f28c044c6a58088a7f1949df829a3e898
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2020-06-06 23:16:36 +0200

    test(user): add UserToUserApi tests

---
+++ src/test/java/io/github/notaphplover/catanserver/user/port/api/UserToUserApiPortTest.java
@@ -0,0 +1,62 @@
+package io.github.notaphplover.catanserver.user.port.api;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+import io.github.notaphplover.catanserver.user.adapter.api.model.IUserApi;
+import io.github.notaphplover.catanserver.user.adapter.jwt.JwtManager;
+import io.github.notaphplover.catanserver.user.adapter.model.UserApiFixturesUtils;
+import io.github.notaphplover.catanserver.user.domain.model.UserFixturesUtils;
+import org.junit.jupiter.api.*;
+import org.mockito.Mockito;
+
+@DisplayName("UserToUserApiPort tests")
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class UserToUserApiPortTest {
+
+  private JwtManager jwtManager = null;
+
+  private UserToUserApiPort userToUserApiPort = null;
+
+  @BeforeAll
+  public void beforeAll() {
+
+    jwtManager = Mockito.mock(JwtManager.class);
+
+    userToUserApiPort = new UserToUserApiPort(jwtManager);
+  }
+
+  @DisplayName("PostUserRequestToUserCreationQueryPort.transform")
+  @Nested
+  @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+  class Transform {
+
+    @BeforeAll
+    public void beforeAll() {
+      when(jwtManager.generateToken(any()))
+          .thenReturn(UserApiFixturesUtils.getUserApiFactory().get().getToken());
+    }
+
+    @DisplayName("PostUserRequestToUserCreationQueryPort.transform, when called")
+    @Nested
+    @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+    class WhenCalled {
+
+      private Object result = null;
+
+      @BeforeAll
+      public void beforeAll() {
+        result = userToUserApiPort.transform(UserFixturesUtils.getUserFactory().get());
+      }
+
+      @DisplayName("It must return an UserCreationQuery")
+      @Test
+      public void itMustReturnACreationQuery() {
+        IUserApi expected = UserApiFixturesUtils.getUserApiFactory().get();
+
+        assertEquals(expected, result);
+      }
+    }
+  }
+}
