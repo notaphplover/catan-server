1d86bfe63f1d79fda52bb86d7acf20a1dc18b286
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2020-06-06 22:10:06 +0200

    test(user) add PostUserRequestToUserCreationQueryPort tests

---
+++ src/test/java/io/github/notaphplover/catanserver/user/port/api/PostUserRequestToUserCreationQueryPortTest.java
@@ -0,0 +1,50 @@
+package io.github.notaphplover.catanserver.user.port.api;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import io.github.notaphplover.catanserver.user.adapter.api.request.PostUserRequestFixturesUtils;
+import io.github.notaphplover.catanserver.user.domain.query.UserCreationQuery;
+import io.github.notaphplover.catanserver.user.domain.query.UserCreationQueryFixturesUtils;
+import org.junit.jupiter.api.*;
+
+@DisplayName("PostUserRequestToUserCreationQueryPort tests")
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class PostUserRequestToUserCreationQueryPortTest {
+
+  private PostUserRequestToUserCreationQueryPort postUserRequestToUserCreationQueryPort = null;
+
+  @BeforeAll
+  public void beforeAll() {
+    postUserRequestToUserCreationQueryPort = new PostUserRequestToUserCreationQueryPort();
+  }
+
+  @DisplayName("PostUserRequestToUserCreationQueryPort.transform")
+  @Nested
+  @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+  class Transform {
+
+    @DisplayName("PostUserRequestToUserCreationQueryPort.transform, when called")
+    @Nested
+    @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+    class WhenCalled {
+
+      private Object result = null;
+
+      @BeforeAll
+      public void beforeAll() {
+        result =
+            postUserRequestToUserCreationQueryPort.transform(
+                PostUserRequestFixturesUtils.postUserRequestFactory().get());
+      }
+
+      @DisplayName("It must return an UserCreationQuery")
+      @Test
+      public void itMustReturnACreationQuery() {
+        UserCreationQuery expected =
+            UserCreationQueryFixturesUtils.getUserCreationQueryFactory().get();
+
+        assertEquals(expected, result);
+      }
+    }
+  }
+}
