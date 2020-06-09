9d79a0ce8189e57ba68922aff7094a591af02848
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2020-06-09 22:36:21 +0200

    test(user): add UserTokenJwtToUserTokenPort tests

---
+++ src/test/java/io/github/notaphplover/catanserver/user/port/jwt/UserTokenJwtToUserTokenPortTest.java
@@ -0,0 +1,47 @@
+package io.github.notaphplover.catanserver.user.port.jwt;
+
+import io.github.notaphplover.catanserver.user.adapter.jwt.model.UserTokenJwtFixturesUtils;
+import io.github.notaphplover.catanserver.user.domain.model.IUserToken;
+import io.github.notaphplover.catanserver.user.domain.model.UserTokenFixturesUtils;
+import org.junit.jupiter.api.*;
+
+@DisplayName("UserTokenJwtToUserTokenPort test")
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class UserTokenJwtToUserTokenPortTest {
+
+  private UserTokenJwtToUserTokenPort userTokenJwtToUserTokenPort = null;
+
+  @BeforeAll
+  public void beforeAll() {
+    userTokenJwtToUserTokenPort = new UserTokenJwtToUserTokenPort();
+  }
+
+  @DisplayName("UserTokenJwtToUserTokenPort.transform")
+  @Nested
+  @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+  class Transform {
+
+    @DisplayName("UserTokenJwtToUserTokenPort.transform, when called")
+    @Nested
+    @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+    class WhenCalled {
+
+      Object result = null;
+
+      @BeforeAll
+      public void beforeAll() {
+        result =
+            userTokenJwtToUserTokenPort.transform(
+                UserTokenJwtFixturesUtils.getUserTokenJwtFactory().get());
+      }
+
+      @DisplayName("It must return an user token")
+      @Test
+      public void itMustReturnAUserToken() {
+        IUserToken expected = UserTokenFixturesUtils.getUserTokenFactory().get();
+
+        Assertions.assertEquals(expected, result);
+      }
+    }
+  }
+}
