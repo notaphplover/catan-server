a5058fb94c440451730f684be1461f5a489ba141
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2020-06-19 12:13:28 +0200

    fix(user): add generated ids to exceptions

--- src/main/java/io/github/notaphplover/catanserver/user/adapter/jwt/exception/ExpiredTokenException.java
+++ src/main/java/io/github/notaphplover/catanserver/user/adapter/jwt/exception/ExpiredTokenException.java
@@ -2,6 +2,8 @@ package io.github.notaphplover.catanserver.user.adapter.jwt.exception;
 
 public class ExpiredTokenException extends RuntimeException {
 
+  private static final long serialVersionUID = 3538840686572908150L;
+
   public ExpiredTokenException(Throwable cause) {
     super(cause);
   }
--- src/main/java/io/github/notaphplover/catanserver/user/adapter/jwt/exception/WrongSignatureTokenException.java
+++ src/main/java/io/github/notaphplover/catanserver/user/adapter/jwt/exception/WrongSignatureTokenException.java
@@ -2,6 +2,8 @@ package io.github.notaphplover.catanserver.user.adapter.jwt.exception;
 
 public class WrongSignatureTokenException extends RuntimeException {
 
+  private static final long serialVersionUID = -2659113709708157663L;
+
   public WrongSignatureTokenException(Throwable cause) {
     super(cause);
   }
