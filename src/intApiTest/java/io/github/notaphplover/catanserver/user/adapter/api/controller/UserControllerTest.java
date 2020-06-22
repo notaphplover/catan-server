49941c52fd94aabcc5654fbbda36e54081fd3eb3
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2020-06-22 23:54:55 +0200

    test(user): add tests for UserController.newUser

--- src/intApiTest/java/io/github/notaphplover/catanserver/user/adapter/api/controller/UserControllerTest.java
+++ src/intApiTest/java/io/github/notaphplover/catanserver/user/adapter/api/controller/UserControllerTest.java
@@ -6,8 +6,10 @@ import io.github.notaphplover.catanserver.CatanServerApplication;
 import io.github.notaphplover.catanserver.common.adapter.api.exception.EntityNotFoundException;
 import io.github.notaphplover.catanserver.common.adapter.api.reqHandler.IReqHandler;
 import io.github.notaphplover.catanserver.fixtures.user.adapter.api.model.UserApiFixturesUtils;
+import io.github.notaphplover.catanserver.fixtures.user.adapter.api.request.PostUserRequestFixturesUtils;
 import io.github.notaphplover.catanserver.user.adapter.api.model.IUserApi;
 import io.github.notaphplover.catanserver.user.adapter.api.request.GetUserRequest;
+import io.github.notaphplover.catanserver.user.adapter.api.request.PostUserRequest;
 import java.util.Optional;
 import org.junit.jupiter.api.*;
 import org.mockito.ArgumentMatchers;
@@ -16,6 +18,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -32,6 +35,8 @@ public class UserControllerTest {
   @Autowired private MockMvc mockMvc;
 
   @MockBean private IReqHandler<GetUserRequest, Optional<IUserApi>> getUserRequestHandler;
+
+  @MockBean private IReqHandler<PostUserRequest, IUserApi> postUserReqHandler;
 
   @DisplayName("UserController.getMyUser")
   @Nested
@@ -122,6 +127,58 @@ public class UserControllerTest {
 
         MockMvcResultMatchers.status().isNotFound().match(result);
         MockMvcResultMatchers.jsonPath("$.msg").value(errorMessage).match(result);
+      }
+    }
+  }
+
+  @DisplayName("UserController.newUser")
+  @Nested
+  @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+  class NewUser {
+
+    @DisplayName("UserController.newUser, when called")
+    @Nested
+    @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+    class WhenCalled {
+      private MvcResult result;
+
+      private String token;
+
+      @BeforeAll
+      public void beforeAll() throws Exception {
+
+        Mockito.when(postUserReqHandler.handle(ArgumentMatchers.any()))
+            .thenReturn(UserApiFixturesUtils.getUserApiFactory().get());
+
+        token = UserApiFixturesUtils.getUserApiFactory().get().getToken();
+
+        PostUserRequest postUserRequest =
+            PostUserRequestFixturesUtils.postUserRequestFactory().get();
+
+        String requestContent =
+            String.format(
+                "{ \"username\": \"%s\", \"password\": \"%s\" }",
+                postUserRequest.getUsername(), postUserRequest.getPassword());
+
+        result =
+            mockMvc
+                .perform(
+                    MockMvcRequestBuilders.post("/users")
+                        .header("Authorization", "Bearer " + token)
+                        .content(requestContent)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andReturn();
+      }
+
+      @DisplayName("It must return its user")
+      @Test
+      public void itMustReturnItsUser() throws Exception {
+
+        IUserApi userApi = UserApiFixturesUtils.getUserApiFactory().get();
+
+        MockMvcResultMatchers.status().isOk().match(result);
+        MockMvcResultMatchers.jsonPath("$.username").value(userApi.getUsername()).match(result);
+        MockMvcResultMatchers.jsonPath("$.token").value(userApi.getToken()).match(result);
       }
     }
   }
